78e6e4aac1965375a66a75984cad08926a1d5777
thuitafaith <thuitamuthoni15@gmail.com>
2019-01-29 15:53:35 +0300

    Updates

---
+++ precache-manifest.18af0e725fd1bbd405e5bb311b27abe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5526b99c40c0fc64eaa5",
+    "url": "thuitafaith.github.io/Meme-Generator/static/js/runtime~main.5526b99c.js"
+  },
+  {
+    "revision": "e475d6576982b6fdba28",
+    "url": "thuitafaith.github.io/Meme-Generator/static/js/main.e475d657.chunk.js"
+  },
+  {
+    "revision": "e432fe6fd1a8bc4af15e",
+    "url": "thuitafaith.github.io/Meme-Generator/static/js/1.e432fe6f.chunk.js"
+  },
+  {
+    "revision": "e475d6576982b6fdba28",
+    "url": "thuitafaith.github.io/Meme-Generator/static/css/main.2b29703b.chunk.css"
+  },
+  {
+    "revision": "4d3b2de28922bf7d75ea771330ce3794",
+    "url": "thuitafaith.github.io/Meme-Generator/index.html"
+  }
+];

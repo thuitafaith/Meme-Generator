00fb381d07dd081776fc7307bc04cf9890c8fafe
thuitafaith <thuitamuthoni15@gmail.com>
2019-01-29 15:43:29 +0300

    Create new files

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"/>
+            <h1>MEME GENERATOR</h1>
+        </header>
+    )
+}
+
+
+export default Header 
